c7a079052899f33cfc65d213f47c5e4e4165bcfc
hfaheem <hfaheem@uwaterloo.ca>
2024-09-15 17:25:33 -0400

    sign in screen ui implemented

--- app/src/main/java/com/haseeb/measuremate/ui/signin/SignInScreen.kt
+++ app/src/main/java/com/haseeb/measuremate/ui/signin/SignInScreen.kt
@@ -1,34 +1,55 @@
 package com.haseeb.measuremate.ui.signin
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.haseeb.measuremate.R
+import com.haseeb.measuremate.ui.component.AnonymousSignInButton
+import com.haseeb.measuremate.ui.component.GoogleSignInButton
 import com.haseeb.measuremate.ui.theme.MeasureMateTheme
 
 
 @Composable
 fun SignInScreen(){
-    Column {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
         Image(painter = painterResource(R.drawable.app_logo),
             contentDescription = "Application Logo",
             modifier = Modifier.size(120.dp)
         )
+        Spacer(modifier = Modifier.height(20.dp))
         Text(text = "Measure Mate",
             style = MaterialTheme.typography.headlineLarge
            )
         Text(text = "Measure progress, not perfection",
             style = MaterialTheme.typography.bodySmall.copy(fontStyle =  FontStyle.Italic)
         )
+        Spacer(modifier = Modifier.fillMaxHeight( 0.4f))
 
+        GoogleSignInButton(
+            onClick = {}
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        AnonymousSignInButton(
+            onClick = {}
+        )
 
     }
 }
@@ -39,6 +60,4 @@ fun SignInScreen(){
 @Composable
 private fun SignInScreenPreview(){
     MeasureMateTheme {  SignInScreen() }
-
-
 }
