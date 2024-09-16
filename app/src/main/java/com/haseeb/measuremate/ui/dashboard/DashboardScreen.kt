f871ee6d8b7ced499addeecfdcfbd8f5952092cc
Haseeb <23100044@lums.edu.pk>
2024-09-16 18:43:05 -0400

    profile bottom sheet added

--- app/src/main/java/com/haseeb/measuremate/ui/dashboard/DashboardScreen.kt
+++ app/src/main/java/com/haseeb/measuremate/ui/dashboard/DashboardScreen.kt
@@ -27,7 +27,12 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -35,12 +40,35 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.haseeb.measuremate.domain.model.BodyPart
+import com.haseeb.measuremate.domain.model.User
 import com.haseeb.measuremate.domain.model.predefinedBodyParts
+import com.haseeb.measuremate.ui.component.ProfileBottomSheet
 import com.haseeb.measuremate.ui.component.ProfilePicPlaceholder
 import com.haseeb.measuremate.ui.theme.MeasureMateTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DashboardScreen(){
+
+    var isProfileBottomSheetOpen by remember {
+        mutableStateOf(false)
+    }
+    val user = User(
+        name = "Haseeb",
+        email = "haseebrehman208@gmail.com",
+        profilePictureUrl = "",
+        isAnonymous = false
+    )
+    ProfileBottomSheet(
+        isOpen = isProfileBottomSheetOpen,
+        user = user,
+        onDismissRequest = {
+            isProfileBottomSheetOpen = false
+        },
+        buttonLoadingState = false,
+        buttonPrimaryText = "Sign out with Google",
+        onGoogleButtonClick = {}
+    )
 
     Box(
         modifier = Modifier.fillMaxSize()
@@ -49,8 +77,10 @@ fun DashboardScreen(){
             modifier = Modifier.fillMaxSize()
         ) {
             DashboardTopBar(
-                profilePictureUrl = null,
-                onProfilePicClick = {}
+                profilePictureUrl = user.profilePictureUrl,
+                onProfilePicClick = {
+                    isProfileBottomSheetOpen = true
+                }
             )
             LazyVerticalGrid(
                 modifier = Modifier.fillMaxSize(),
@@ -69,8 +99,9 @@ fun DashboardScreen(){
         }
         FloatingActionButton(
             onClick = { },
-            modifier = Modifier.align(Alignment.BottomEnd).
-            padding(24.dp)
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(24.dp)
         ) {
             Icon(
                 imageVector = Icons.Default.Add,
